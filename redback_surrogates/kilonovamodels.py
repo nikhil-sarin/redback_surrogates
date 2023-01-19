c7e38265947073ce73a45640007cfd202da8fe69
Nikhil Sarin <nsarin.astro@gmail.com>
2023-01-19 17:57:15 +0100

    extra explicit docs

--- redback_surrogates/kilonovamodels.py
+++ redback_surrogates/kilonovamodels.py
@@ -16,7 +16,8 @@ def bulla_bns_kilonovanet_spectra(time_source_frame, redshift, mej_dyn, mej_disk
     :param mej_disk: disk mass of ejecta in solar masses
     :param phi: half-opening angle of the lanthanide-rich tidal dynamical ejecta in degrees
     :param costheta_obs: cosine of the observers viewing angle
-    :return: named tuple with observer frame time, observer frame wavelength, and spectra in observer frame
+    :return: named tuple with observer frame time in days, observer frame wavelength in Angstroms,
+    and spectra in observer frame in erg/s/Angstrom
     """
     metadata_file = f"{dirname}/surrogate_data/metadata_bulla_bns.json"
     torch_file = f"{dirname}/surrogate_data/bulla-bns-latent-20-hidden-1000-CV-4.pt"
@@ -25,8 +26,8 @@ def bulla_bns_kilonovanet_spectra(time_source_frame, redshift, mej_dyn, mej_disk
     physical_parameters = np.array([mej_dyn, mej_disk, phi, costheta_obs])
     model = knnet.Model(metadata_file, torch_file)
     spectra, unique_t = model.predict_spectra(physical_parameters, time_source_frame)
-    return namedtuple('output', ['time', 'frequency', 'spectra'])(time=time_observer_frame,
-                                                                  frequency=wavelength_observer_frame,
+    return namedtuple('output', ['time', 'lambdas', 'spectra'])(time=time_observer_frame,
+                                                                  lambdas=wavelength_observer_frame,
                                                                   spectra=spectra)
 @citation_wrapper('https://ui.adsabs.harvard.edu/abs/2022MNRAS.516.1137L/abstract')
 def bulla_nsbh_kilonovanet_spectra(time_source_frame, redshift, mej_dyn, mej_disk, costheta_obs, **kwargs):
@@ -38,7 +39,8 @@ def bulla_nsbh_kilonovanet_spectra(time_source_frame, redshift, mej_dyn, mej_dis
     :param mej_dyn: dynamical mass of ejecta in solar masses
     :param mej_disk: disk mass of ejecta in solar masses
     :param costheta_obs: cosine of the observers viewing angle
-    :return: named tuple with observer frame time, observer frame wavelength, and spectra in observer frame
+    :return: named tuple with observer frame time in days, observer frame wavelength in Angstroms,
+    and spectra in observer frame in erg/s/Angstrom
     """
     metadata_file = f"{dirname}/surrogate_data/metadata_bulla_bhns.json"
     torch_file = f"{dirname}/surrogate_data/bulla-bhns-latent-2-hidden-500-CV-4.pt"
@@ -47,8 +49,8 @@ def bulla_nsbh_kilonovanet_spectra(time_source_frame, redshift, mej_dyn, mej_dis
     physical_parameters = np.array([mej_dyn, mej_disk, costheta_obs])
     model = knnet.Model(metadata_file, torch_file)
     spectra, unique_t = model.predict_spectra(physical_parameters, time_source_frame)
-    return namedtuple('output', ['time', 'frequency', 'spectra'])(time=time_observer_frame,
-                                                                  frequency=wavelength_observer_frame,
+    return namedtuple('output', ['time', 'lambdas', 'spectra'])(time=time_observer_frame,
+                                                                  lambdas=wavelength_observer_frame,
                                                                   spectra=spectra)
 @citation_wrapper('https://ui.adsabs.harvard.edu/abs/2022MNRAS.516.1137L/abstract')
 def kasen_bns_kilonovanet_spectra(time_source_frame, redshift, mej, vej, chi, **kwargs):
@@ -60,7 +62,8 @@ def kasen_bns_kilonovanet_spectra(time_source_frame, redshift, mej, vej, chi, **
     :param mej: ejecta mass in solar masses
     :param vej: ejecta velocity in units of c
     :param chi: lanthanide fraction
-    :return: named tuple with observer frame time, observer frame wavelength, and spectra in observer frame
+    :return: named tuple with observer frame time in days, observer frame wavelength in Angstroms,
+    and spectra in observer frame in erg/s/Angstrom
     """
     metadata_file = f"{dirname}/surrogate_data/metadata_kasen_bns.json"
     torch_file = f"{dirname}/surrogate_data/kasen-bns-latent-10-hidden-500-CV-4.pt"
@@ -69,6 +72,6 @@ def kasen_bns_kilonovanet_spectra(time_source_frame, redshift, mej, vej, chi, **
     physical_parameters = np.array([mej, vej, chi])
     model = knnet.Model(metadata_file, torch_file)
     spectra, unique_t = model.predict_spectra(physical_parameters, time_source_frame)
-    return namedtuple('output', ['time', 'frequency', 'spectra'])(time=time_observer_frame,
-                                                                  frequency=wavelength_observer_frame,
+    return namedtuple('output', ['time', 'lambdas', 'spectra'])(time=time_observer_frame,
+                                                                  lambdas=wavelength_observer_frame,
                                                                   spectra=spectra)
