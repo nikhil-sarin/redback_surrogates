e1355536c27968b4b7dcd37343d467405a74a860
Jeremy Kubica <104161096+jeremykubica@users.noreply.github.com>
2025-11-14 09:29:16 -0500

    Add a basic data set wrapper

---
+++ redback_surrogates/learned_surrogate_data.py
@@ -0,0 +1,205 @@
+"""This model defines a wrapper data set class for training LearnedSurrogateModels.
+
+These models hold a table of parameters and associated grid data, which can either
+be stored directly in the table or as references to external files (e.g., NumPy files).
+"""
+
+from astropy.table import Table
+from pathlib import Path
+
+import numpy as np
+
+
+class LearnedSurrogateDataset:
+    """A base class for datasets representing functions where the input is a set of
+    parameters and the output is a grid of values.
+
+    The dataset is defined by a parameter table (e.g., CSV, Parquet) where each row
+    corresponds to a unique set of parameters and a grid of values associated with those
+    parameters. The grid data can be stored directly in the table or as references to
+    external files (e.g., NumPy files).
+
+    :param data: An astropy Table containing the parameter sets and associated grid data.
+    :param output_column: An optional string with the name of the Table column that contains
+        the grid data or references to it.
+    :param times: An optional array-like object representing the time points associated with
+        the grid data.
+    :param wavelengths: An optional array-like object representing the wavelength points
+        associated with the grid data.
+    """
+
+    def __init__(self, data, output_column="filename", times=None, wavelengths=None):
+        self.data = data
+        self._output_column = output_column
+        if output_column not in data.colnames:
+            raise ValueError(f"Grid column '{output_column}' not found in the dataset.")
+        self._in_files = np.issubdtype(data[output_column].dtype, np.str_)
+        self.parameter_names = [col for col in data.colnames if col != output_column]
+        self.times = times
+        self.wavelengths = wavelengths
+
+    def __len__(self):
+        return len(self.data)
+
+    @property
+    def in_memory(self):
+        """Returns True if the output data is stored directly in the table, False if it is
+        stored as file references.
+        """
+        return not self._in_files
+
+    @classmethod
+    def from_file(cls, filename: str | Path):
+        """Creates a LearnedSurrogateDataset from a file.
+
+        :param filename: A string or Path specifying the path to the dataset file.
+        :param output_column: An optional string with the name of the Table column that contains
+            the grid data or references to it.
+        :return: An instance of LearnedSurrogateDataset.
+        """
+        filename = Path(filename)
+        if not filename.exists():
+            raise FileNotFoundError(f"Unable to find the data file: {filename}")
+        data = Table.read(filename)
+
+        # Load load the metadata that is present in the file, if it exists.
+        output_column = data.meta.get("output_column", "filename")
+        times = data.meta.get("times", None)
+        if times is not None:
+            times = np.asarray(times)
+        wavelengths = data.meta.get("wavelengths", None)
+        if wavelengths is not None:
+            wavelengths = np.asarray(wavelengths)
+        return cls(
+            data, output_column=output_column, times=times, wavelengths=wavelengths
+        )
+
+    def get_input(self, idx):
+        """Retrieves the input parameters for a given index.
+
+        :param idx: An integer or slice specifying the index of the desired data point.
+        :return: A numpy array containing the parameter values as columns (in the same order
+            as the parameter names).
+        """
+        if np.isscalar(idx):
+            return np.asarray([self.data[param][idx] for param in self.parameter_names])
+        else:
+            return np.column_stack(
+                [self.data[param][idx] for param in self.parameter_names]
+            )
+
+    def get_input_tensors(self, idx):
+        """Retrieves the input parameters for a given index.
+
+        :param idx: An integer or slice specifying the index of the desired data point.
+        :return: A list of tensors containing the parameter values as columns (in the same order
+            as the parameter names).
+        """
+        try:
+            import torch
+        except ImportError as err:
+            raise ImportError("PyTorch is required to use this method.") from err
+
+        if np.isscalar(idx):
+            return [
+                torch.tensor(self.data[param][idx]) for param in self.parameter_names
+            ]
+        else:
+            return [
+                torch.tensor(self.data[param][idx].value)
+                for param in self.parameter_names
+            ]
+
+    def get_output(self, idx):
+        """Retrieves the output (grid) data for a given index or indices.
+
+        :param idx: An integer or slice specifying the index of the desired data point.
+        :return: A NumPy array containing the grid data associated with the specified index.
+        """
+        if np.isscalar(idx):
+            row_data = [self.data[self._output_column][idx]]
+            single_index = True
+        else:
+            row_data = self.data[self._output_column][idx]
+            single_index = False
+
+        # If the output column gives file names, load the data from those files.
+        if self._in_files:
+            filenames = row_data
+            row_data = []
+            for idx, filename in enumerate(filenames):
+                row_data.append(np.load(filename))
+
+        # Return a single array if the input index is a scalar, otherwise return a
+        # multi-dimensional array with all the data.
+        if single_index:
+            return np.asarray(row_data[0])
+        return np.asarray(row_data)
+
+    def load_output_to_memory(self):
+        """Loads all output data into memory if it is currently stored as file references.
+
+        This is useful if the output data is stored as file references and we want to
+        load it all at once for faster access.
+        """
+        if not self._in_files:
+            return  # Output data is already in memory
+
+        # Read each file into memory and save it as a new column in the table.
+        new_output_column = f"{self._output_column}_loaded"
+        loaded_data = []
+        for idx in range(len(self.data)):
+            filename = self.data[self._output_column][idx]
+            loaded_data.append(np.load(filename))
+        self.data[new_output_column] = loaded_data
+
+        # Update the output column name and mark it as loaded.
+        self._output_column = new_output_column
+        self._in_files = False
+
+    def export_output_to_files(self, file_prefix):
+        """Writes all the output data to separate files and updates the dataset
+        to reference those files.
+
+        :param file_prefix: A string specifying the prefix for the output files. The files
+            will be named as "{file_prefix}_{idx}.npy" where idx is the index of the data point.
+        """
+        if self._in_files:
+            return  # Output data is already in separate files
+
+        filenames = []
+        for idx in range(len(self.data)):
+            output_data = self.data[self._output_column][idx]
+            filename = f"{file_prefix}_{idx}.npy"
+            print(f"Saving output data for index {idx} to file: {filename}")
+            np.save(filename, output_data)
+            filenames.append(filename)
+        self.data[self._output_column] = filenames
+        self._in_files = True
+
+    def save(self, filename, overwrite=False, separate_files=False):
+        """Saves the dataset to a file.
+
+        :param filename: A string or Path specifying the path to save the dataset file.
+        :param overwrite: A boolean indicating whether to overwrite the file if it already exists.
+        :param separate_files: A boolean indicating whether to save the output grids as separate
+            files (e.g., NumPy files) instead of directly in the table. If True, the output
+            grids will be saved as separate files and the table will contain references to those
+            files.
+        """
+        filename = Path(filename)
+        if filename.exists() and not overwrite:
+            raise FileExistsError(f"File already exists: {filename}")
+
+        # Handle converting to/from separate files.
+        if not separate_files:
+            self.load_output_to_memory()
+        else:
+            self.export_output_to_files(file_prefix=filename.parent / filename.stem)
+
+        self.data.meta["output_column"] = self._output_column
+        if self.times is not None:
+            self.data.meta["times"] = self.times.tolist()
+        if self.wavelengths is not None:
+            self.data.meta["wavelengths"] = self.wavelengths.tolist()
+        self.data.write(filename, overwrite=True)
---
+++ test/test_learned_surrogate_data.py
@@ -0,0 +1,190 @@
+import numpy as np
+import tempfile
+import unittest
+
+from astropy.table import Table
+from pathlib import Path
+
+from redback_surrogates.learned_surrogate_data import LearnedSurrogateDataset
+
+
+class TestLearnedSurrogateDataset(unittest.TestCase):
+
+    def test_learned_surrogate_dataset_basic(self):
+        """Test that we can create a basic LearnedSurrogateDataset."""
+        data = Table(
+            {
+                "param1": [1, 2, 3],
+                "param2": [4, 5, 6],
+                "grids": [
+                    np.array([[0.1, 0.2, 0.3], [0.3, 0.4, 5.0]]),
+                    np.array([[0.5, 0.6, 0.7], [0.7, 0.8, 0.9]]),
+                    np.array([[0.9, 1.0, 1.1], [1.1, 1.2, 1.3]]),
+                ],
+            }
+        )
+        dataset = LearnedSurrogateDataset(data, output_column="grids")
+        assert len(dataset) == 3
+        assert dataset.parameter_names == ["param1", "param2"]
+        assert dataset._output_column == "grids"
+        assert dataset.times is None
+        assert dataset.wavelengths is None
+
+        # We can access slices of the input parameters.
+        assert np.array_equal(dataset.get_input(0), np.array([1, 4]))
+        assert np.array_equal(dataset.get_input([0, 2]), np.array([[1, 4], [3, 6]]))
+
+        # We can access the output grids.
+        assert np.array_equal(
+            dataset.get_output(0), np.array([[0.1, 0.2, 0.3], [0.3, 0.4, 5.0]])
+        )
+        assert np.array_equal(
+            dataset.get_output([0, 2]),
+            np.array(
+                [[[0.1, 0.2, 0.3], [0.3, 0.4, 5.0]], [[0.9, 1.0, 1.1], [1.1, 1.2, 1.3]]]
+            ),
+        )
+
+        # We fail if the grid column isn't in the data.
+        with self.assertRaises(ValueError):
+            LearnedSurrogateDataset(data, output_column="nonexistent_column")
+
+        # We can add metadata.
+        times = np.array([0.1, 0.5])
+        wavelengths = np.array([1000.0, 2000.0, 3000.0])
+        dataset = LearnedSurrogateDataset(
+            data, output_column="grids", times=times, wavelengths=wavelengths
+        )
+        assert np.array_equal(dataset.times, times)
+        assert np.array_equal(dataset.wavelengths, wavelengths)
+
+    def test_learned_surrogate_dataset_to_from_file(self):
+        """Test that we can save and load a LearnedSurrogateDataset."""
+        data = Table(
+            {
+                "param1": [1, 2, 3],
+                "param2": [4, 5, 6],
+                "grids": [
+                    np.array([[0.1, 0.2, 0.3], [0.3, 0.4, 5.0]]),
+                    np.array([[0.5, 0.6, 0.7], [0.7, 0.8, 0.9]]),
+                    np.array([[0.9, 1.0, 1.1], [1.1, 1.2, 1.3]]),
+                ],
+            }
+        )
+        times = np.array([0.1, 0.5, 0.6, 0.7, 0.8, 0.9])
+        wavelengths = np.array([1000.0, 2000.0, 3000.0])
+        dataset = LearnedSurrogateDataset(
+            data,
+            output_column="grids",
+            times=times,
+            wavelengths=wavelengths,
+        )
+        assert dataset.in_memory
+
+        with tempfile.TemporaryDirectory() as tmpdir:
+            filename = Path(tmpdir) / "dataset.ecsv"
+
+            # The data set doesn't exist yet.
+            assert not filename.exists()
+            with self.assertRaises(FileNotFoundError):
+                LearnedSurrogateDataset.from_file(filename)
+
+            # Save the dataset and check that it exists.
+            dataset.save(filename)
+            assert filename.exists()
+
+            # Load the dataset and check that it matches the original, including the metadata.
+            loaded_dataset = LearnedSurrogateDataset.from_file(filename)
+            assert loaded_dataset.in_memory
+            assert len(loaded_dataset) == 3
+            assert loaded_dataset.parameter_names == ["param1", "param2"]
+            assert loaded_dataset._output_column == "grids"
+            assert np.array_equal(loaded_dataset.times, times)
+            assert np.array_equal(loaded_dataset.wavelengths, wavelengths)
+
+            # We can access slices of the input parameters.
+            assert np.array_equal(loaded_dataset.get_input(0), np.array([1, 4]))
+            assert np.array_equal(
+                loaded_dataset.get_input([0, 2]), np.array([[1, 4], [3, 6]])
+            )
+
+            # We can access the output grids.
+            assert np.array_equal(
+                loaded_dataset.get_output(0),
+                np.array([[0.1, 0.2, 0.3], [0.3, 0.4, 5.0]]),
+            )
+            assert np.array_equal(
+                loaded_dataset.get_output([0, 2]),
+                np.array(
+                    [
+                        [[0.1, 0.2, 0.3], [0.3, 0.4, 5.0]],
+                        [[0.9, 1.0, 1.1], [1.1, 1.2, 1.3]],
+                    ]
+                ),
+            )
+
+            # We fail if we try to write again with overwrite=False.
+            with self.assertRaises(FileExistsError):
+                dataset.save(filename, overwrite=False)
+
+    def test_learned_surrogate_dataset_to_from_separate_files(self):
+        """Test that we can save and load a LearnedSurrogateDataset using separate files."""
+        data = Table(
+            {
+                "param1": [1, 2, 3],
+                "param2": [4, 5, 6],
+                "grids": [
+                    np.array([[0.1, 0.2, 0.3], [0.3, 0.4, 5.0]]),
+                    np.array([[0.5, 0.6, 0.7], [0.7, 0.8, 0.9]]),
+                    np.array([[0.9, 1.0, 1.1], [1.1, 1.2, 1.3]]),
+                ],
+            }
+        )
+        times = np.array([0.1, 0.5, 0.6, 0.7, 0.8, 0.9])
+        wavelengths = np.array([1000.0, 2000.0, 3000.0])
+        dataset = LearnedSurrogateDataset(
+            data,
+            output_column="grids",
+            times=times,
+            wavelengths=wavelengths,
+        )
+        assert dataset.in_memory
+
+        with tempfile.TemporaryDirectory() as tmpdir:
+            filename = Path(tmpdir) / "dataset_sep.ecsv"
+            dataset.save(filename, separate_files=True)
+
+            # The main data set file and each output file should exist.
+            assert filename.exists()
+            for idx in range(len(dataset)):
+                assert (filename.parent / f"{filename.stem}_{idx}.npy").exists()
+
+            # Load the dataset and check that it matches the original, including the metadata.
+            loaded_dataset = LearnedSurrogateDataset.from_file(filename)
+            assert not loaded_dataset.in_memory
+            assert len(loaded_dataset) == 3
+            assert loaded_dataset.parameter_names == ["param1", "param2"]
+            assert loaded_dataset._output_column == "grids"
+            assert np.array_equal(loaded_dataset.times, times)
+            assert np.array_equal(loaded_dataset.wavelengths, wavelengths)
+
+            # We can access slices of the input parameters.
+            assert np.array_equal(loaded_dataset.get_input(0), np.array([1, 4]))
+            assert np.array_equal(
+                loaded_dataset.get_input([0, 2]), np.array([[1, 4], [3, 6]])
+            )
+
+            # We can access the output grids.
+            assert np.array_equal(
+                loaded_dataset.get_output(0),
+                np.array([[0.1, 0.2, 0.3], [0.3, 0.4, 5.0]]),
+            )
+            assert np.array_equal(
+                loaded_dataset.get_output([0, 2]),
+                np.array(
+                    [
+                        [[0.1, 0.2, 0.3], [0.3, 0.4, 5.0]],
+                        [[0.9, 1.0, 1.1], [1.1, 1.2, 1.3]],
+                    ]
+                ),
+            )
