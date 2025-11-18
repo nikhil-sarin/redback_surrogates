ed2ab7fca00e8ceb40db2eed05a96c16ac168679
Jeremy Kubica <104161096+jeremykubica@users.noreply.github.com>
2025-11-18 10:11:47 -0500

    Add ability to scale the output during training

--- redback_surrogates/learned_surrogate_train.py
+++ redback_surrogates/learned_surrogate_train.py
@@ -64,6 +64,7 @@ def train_pytorch_model(
     dataset,
     hidden_sizes=[64, 64],
     training_epochs=100,
+    scale_output=True,
 ):
     """Trains a simple neural network surrogate model using PyTorch.
 
@@ -76,6 +77,9 @@ def train_pytorch_model(
     hidden_sizes : int or list of int, optional
         The size(s) of the hidden layers in the neural network.
         Default is a pair of 64 node layers.
+    scale_output : bool, optional
+        Whether to scale the output values to be in the range [0, 1] during training.
+        Default is True.
 
     Returns
     -------
@@ -92,13 +96,25 @@ def train_pytorch_model(
     ]
     output = torch.tensor(dataset.get_output(), dtype=torch.float64)
 
+    # Scale the output values to be in the range [0, 1] for training.
+    if scale_output:
+        output_max = torch.max(output)
+        output_min = torch.min(output)
+        output_scale = output_max - output_min
+        if output_scale == 0:
+            output_scale = 1.0
+        output_shift = output_min
+        output = (output - output_shift) / output_scale
+    else:
+        output_scale = 1.0
+        output_shift = 0.0
+
     # Configure the model and training.
     model = MultilevelSigmoid(
         input_size=len(dataset.parameter_names),
         hidden_sizes=hidden_sizes,
         output_shape=output.shape[1:],
     )
-    print(list(model.parameters()))
     criterion = nn.MSELoss()  # Mean Squared Error for regression
     optimizer = torch.optim.Adam(model.parameters(), lr=0.1)
 
@@ -120,11 +136,103 @@ def train_pytorch_model(
         input_example,
         input_names=dataset.parameter_names,
         dynamo=True,
-    )
+    ).model_proto
+
+    # Add layers to the ONNX model to undo the scaling.
+    if scale_output:
+        onnx_program = add_output_scaling_and_shift_layers(
+            onnx_program,
+            scaling_factor=output_scale.item(),
+            shift=output_shift.item(),
+        )
+
     surrogate_model = LearnedSurrogateModel(
-        onnx_program.model_proto,
+        onnx_program,
         times=dataset.times,
         wavelengths=dataset.wavelengths,
         param_names=dataset.parameter_names,
     )
     return surrogate_model
+
+
+def add_output_scaling_and_shift_layers(onnx_model, scaling_factor, shift=0.0):
+    """Add a scaling and shift operation to the output of an ONNX model.
+    
+    The transformation applied is: output_scaled = (output * scaling_factor) + shift
+    
+    Parameters
+    ----------
+    onnx_model : onnx.ModelProto
+        The ONNX model to modify.
+    scaling_factor : float
+        The scaling factor to multiply the output by.
+    shift : float, optional
+        The additive shift to apply after scaling. Default is 0.0.
+    
+    Returns
+    -------
+    onnx.ModelProto
+        The modified ONNX model with scaling and shift applied to the output.
+    """
+    try:
+        import onnx
+        from onnx import helper, numpy_helper
+    except ImportError as err:
+        raise ImportError(
+            "The onnx package is required to modify the ONNX model. "
+            "Please install it using 'pip install onnx'."
+        ) from err
+
+    # Get the graph
+    graph = onnx_model.graph
+    
+    # Get the original output name and use it to derive two intermediate output names
+    original_output_name = graph.output[0].name
+    unscaled_output_name = original_output_name + "_unscaled"
+    scaled_output_name = original_output_name + "_scaled"
+
+    # Rename the original output to the intermediate name
+    for node in graph.node:
+        for i, output_name in enumerate(node.output):
+            if output_name == original_output_name:
+                node.output[i] = unscaled_output_name
+
+    # Create a proto for constant tensor for the scaling factor
+    scale_tensor_proto = numpy_helper.from_array(
+        np.array([scaling_factor], dtype=np.float64),
+        name="output_scaling_factor"
+    )
+    graph.initializer.append(scale_tensor_proto)
+
+    # Create a Mul node to multiply the output by the scaling factor
+    mul_node = helper.make_node(
+        'Mul',
+        inputs=[unscaled_output_name, 'output_scaling_factor'],
+        outputs=[scaled_output_name],
+        name='output_scaling'
+    )
+    graph.node.append(mul_node)
+
+    # Create a constant tensor for the shift
+    shift_tensor_proto = numpy_helper.from_array(
+        np.array([shift], dtype=np.float64),
+        name="output_shift"
+    )
+    graph.initializer.append(shift_tensor_proto)
+
+    # Create an Add node to add the shift
+    add_node = helper.make_node(
+        'Add',
+        inputs=[scaled_output_name, 'output_shift'],
+        outputs=[original_output_name],
+        name='output_shift_add'
+    )
+    graph.node.append(add_node)
+
+    # Update the graph output to use the final output name
+    graph.output[0].name = original_output_name
+
+    # Run ONNX checker to validate the modified model
+    onnx.checker.check_model(onnx_model)
+
+    return onnx_model
